fbb72a837913a6c8522a2d31b586922d33fed2e6
davelarde <danielavelarde4@gmail.com>
2023-01-19 16:08:05 -0500

    adding params and useEffect to pokemon details

--- src/routes/PokemonDetails.js
+++ src/routes/PokemonDetails.js
@@ -1,2 +1,19 @@
 import React,  { useEffect, useState } from "react"
 import {useParams} from "react-router-dom";
+
+
+function PokemonDetails(){
+    const params = useParams();
+    const [pokemon, setPokemon] = useState(null);
+
+    useEffect(() =>{
+        fetch('https://pokeapi.co/api/v2/pokemon/${params.name}')
+        .then((res) => res.json())
+        .then((data) => {
+            setPokemon(data);
+        });
+    }, [params.name])
+    
+
+
+}

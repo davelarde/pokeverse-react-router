521e9ac91f1602caa587897436ee5be4ce6a971e
davelarde <danielavelarde4@gmail.com>
2023-01-19 16:17:53 -0500

    adding stats to the Pokemon details

--- src/routes/PokemonDetails.js
+++ src/routes/PokemonDetails.js
@@ -13,7 +13,54 @@ function PokemonDetails(){
             setPokemon(data);
         });
     }, [params.name])
-    
 
-
+    if (!pokemon)
+    return <>loading....</>
 }
+return (
+    <div>
+        <img 
+        width="300"
+        heigh="300"
+        src={'https://img.pokemondb.net/artwork/large/${params.name}.jpg'}
+        />
+<h1>{params.name}</h1>
+      <p>height: {pokemon.height}</p>
+      <p>weight: {pokemon.weight}</p>
+      <div>
+        <p>abilities:</p>
+        <ul>
+          {pokemon.abilities.map((ability) => (
+            <li key={ability.ability.name}>
+              <span>{ability.ability.name}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <p>types:</p>
+        <ul>
+          {pokemon.types.map((type) => (
+            <li key={type.type.name}>
+              <span>{type.type.name}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <p>stats:</p>
+        <ul>
+          {pokemon.stats.map((stat) => (
+            <li key={stat.stat.name}>
+              <span>{stat.stat.name}: </span>
+              <span>{stat.base_stat}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+
+  export {PokemonDetails}
